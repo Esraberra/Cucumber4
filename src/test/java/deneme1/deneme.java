a16d032d199230691645de9afeaf99a89c9135ba
onurenescelik <onurenescelik7@gmail.com>
2021-12-22 00:14:21 +0300

    onur

--- src/test/java/deneme1/deneme.java
+++ src/test/java/deneme1/deneme.java
@@ -4,5 +4,6 @@ public class deneme {
     public static void main(String[] args) {
         System.out.println("onurenes");
         System.out.println("onurenes");
+        System.out.println("onurenes");
     }
 }
