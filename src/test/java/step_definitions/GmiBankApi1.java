f49da96d351b0913d4fd7c2f0b61aad8e4504be0
Esraberra <esraberra6@gmail.com>
2021-12-28 12:48:37 +0100

    testdatas for api

---
+++ src/test/java/step_definitions/GmiBankApi1.java
@@ -0,0 +1,68 @@
+package step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.codehaus.jackson.map.ObjectMapper;
+import pojos.customer;
+import utilities.ConfigurationReader;
+import utilities.WriteToTxt;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import static io.restassured.RestAssured.given;
+
+public class GmiBankApi1 {
+    Response response;
+    String bearerToken= ConfigurationReader.getProperty("api_bearer_Token");
+
+    customer [] customers;
+    String fileName=ConfigurationReader.getProperty("fileNameOfCustomerSSN");
+
+    @Given("Kullanici Api endpointe gidecek {string} datalari alacak")
+    public void kullanici_api_endpointe_gidecek_datalari_alacak(String url) {
+        response=given().headers("Authorization",
+                "Bearer " + bearerToken,
+                "Content-Type",
+                ContentType.JSON,
+                "Accept",ContentType.JSON
+                ).when().get(url).
+                then().contentType(ContentType.JSON).extract().response();
+//response.prettyPrint();
+    }
+
+
+
+
+    @Given("Kullanici müsteri datalari alir")
+    public void kullanici_müsteri_datalari_alir() throws IOException {
+
+        ObjectMapper obj=new ObjectMapper();
+
+customers=obj.readValue(response.asString(), customer [].class);
+
+        for (int i = 0; i < customers.length; i++) {
+            System.out.println(customers[i].getId());
+            System.out.println(customers[i].getFirstName());
+
+        }
+
+        }
+
+
+    @Then("kullanici aldigi datalari kaydeder")
+    public void kullanici_aldigi_datalari_kaydeder() {
+WriteToTxt.saveDataInFileWithSSN(fileName, customers);
+
+    }
+
+    @Then("kullanici Customer datalarini onaylar")
+    public void kullanici_customer_datalarini_onaylar() {
+
+
+
+    }
+
+}
--- src/test/java/step_definitions/booking.java
+++ src/test/java/step_definitions/booking.java
@@ -1,40 +1,92 @@
 package step_definitions;
 
+import com.google.common.collect.Ordering;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.bookingPage;
+import utilities.BrowserUtilities;
 import utilities.ConfigurationReader;
 import utilities.Driver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class booking {
 
-
+bookingPage page=new bookingPage();
+Actions actions=new Actions(Driver.getDriver());
 
     @When("Musteri {string} sayfayı acar")
     public void musteri_sayfayı_acar(String string) {
         Driver.getDriver().get(ConfigurationReader.getProperty("bookingUrl"));
+        page.cerez.click();
+
+
+
     }
 
 
 
-
-    @Given("Anasayfadaki Hotels linkine tıklar")
-    public void anasayfadaki_hotels_linkine_tıklar() {
-
-
-    }
     @Given("{string} bölümüne \"New York” yazar")
     public void bölümüne_new_york_yazar(String city) {
+        BrowserUtilities.waitFor(2);
+        page.search1.sendKeys("New York");
 
 
     }
-    @Given("{int} Aralık ile {int} Aralık tarihleri arasında {string} Yetişkin ve {string} yaşında bir çocuk için hotel araması yapılabilmeli.")
-    public void aralık_ile_aralık_tarihleri_arasında_yetişkin_ve_yaşında_bir_çocuk_için_hotel_araması_yapılabilmeli(Integer int1, Integer int2, String string, String string2) {
 
-    }
     @Then("Sonuc sayfasına gecince {string} butonuna tıklayabilmeli ve sonuçları görebilmelidir.")
     public void sonuc_sayfasına_gecince_butonuna_tıklayabilmeli_ve_sonuçları_görebilmelidir(String string) {
 
+        page.priceLow.click();
+/*
+      List<WebElement> LIST= Driver.getDriver().findElements(By.xpath("//span[@class='fde444d7ef _e885fdc12']"));
+
+      List<String> listeÜrun=new ArrayList<>();
+        for (WebElement e:LIST
+             ) {
+            System.out.println(e.getText());
+        }
+
+ */
+        System.out.println(BrowserUtilities.getElementsText(By.xpath("//span[@class='fde444d7ef _e885fdc12']")));
+
+        BrowserUtilities.waitFor(2);
+
     }
 
+
+
+    @And("yirmi iki Aralık ile yirmibes Aralık tarihleri arasında {string} Yetişkin ve {string} yaşında bir çocuk için hotel araması yapılabilmeli.")
+    public void yirmiIkiAralıkIleYirmibesAralıkTarihleriArasındaYetişkinVeYaşındaBirÇocukIçinHotelAramasıYapılabilmeli(String arg0, String arg1) {
+
+
+        actions.sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.TAB). build().perform();
+        actions  . sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.ENTER).build().perform();
+
+        BrowserUtilities.waitFor(4);
+        Select select=new Select(page.childAge);
+        select.selectByValue("12");
+        BrowserUtilities.waitFor(3);
+
+
+        Select select1=new Select(page.selectCheckinMonth);
+        select1.selectByValue("11-2021");
+        Select selectday=new Select(page.checkinDay);
+        selectday.selectByValue("22");
+
+        Select select2=new Select(page.selectCheckoutMonth);
+        select2.selectByVisibleText("Aralık 2021");
+        Select select3=new Select(page.selectCheckoutDay);
+        select3.selectByVisibleText("26, Pazar");
+        page.ara.click();
+
+    }
 }
